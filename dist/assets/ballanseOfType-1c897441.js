9b6335fea0afbffe00c6c4ec73a0e038b7d51db4
Vertalyt <108869252+Vertalyt@users.noreply.github.com>
2023-07-16 19:52:36 +0300

    Delete dist directory

--- dist/assets/ballanseOfType-1c897441.js
+++
@@ -1,1 +0,0 @@
-function u(r,t){return r.filter(e=>e.type===t).reduce((e,l)=>e+=Number(l.bill),0)}export{u as b};
--- dist/assets/currency.filter-780ad7d5.js
+++
@@ -1,1 +0,0 @@
-function n(r,e="UAH"){return new Intl.NumberFormat("en-US",{style:"currency",currency:e}).format(r)}export{n as f};
