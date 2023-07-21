eaa1ae8b2ab824fcdd009360b7c56c9a4e656f0c
Vertalyt <vertal86@gmail.com>
2023-07-21 19:45:05 +0300

    minor edits

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve, dirname } from 'node:path'
-import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueI18nPlugin({
-      /* options */
-      // locale messages resource pre-compile option
-      include: resolve(dirname(fileURLToPath(import.meta.url)), './path/to/src/locales/**'),
-    }),
-  ],
+  build: {
+    chunkSizeWarningLimit: 1600
+},
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
